c4d184223ed35a72c95af8dbb5e92420df5923dc
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2023-07-12 01:01:51 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import CardResume from './components/CardResume.vue';
+</script>
+
+<template>
+  <div>
+    <CardResume title="Vue" content="Vue is a progressive framework for building user interfaces." />
+    <CardResume />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CardResume.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div>
+    <div className="card">
+    <h1>{{title}}</h1>
+      <p> {{content}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "CardResume",
+    props: {
+        title: {
+            type: String,
+            default: "Title",
+            required: true,
+        },
+        content: {
+            type: String,
+            default: "Content",
+            required: true,
+        }
+    },
+  }
+
+</script>
+
+
+
+<style scoped>
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    padding: 1rem;
+    margin: 1rem 0;
+  }
+
+</style>
